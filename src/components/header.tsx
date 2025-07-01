ed431dec8034f1840ffe3f1c2b6e19a77b2d2a17
abue-ammar <iabueammar@gmail.com>
2025-07-01 14:25:58 +0600

    feat: update header styles and theme colors for improved UI consistency

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,14 @@ import { ThemeToggle } from "./mode-toggle";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-neutral-100 bg-white">
+    <header className="bg-background text-foreground border-border sticky top-0 z-50 w-full border-b">
       <div className="container mx-auto flex h-14 items-center px-4">
         <div className="flex w-full items-center justify-between">
           <div className="mr-4">
             <a className="mr-6 flex items-center space-x-2" href="/">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="size-8"
+                className="text-primary size-8"
                 viewBox="0 0 24 24"
                 strokeWidth={1.8}
                 stroke="currentColor"
@@ -23,7 +23,7 @@ const Header = () => {
                 <path d="M3 16l5 -5c.928 -.893 2.072 -.893 3 0l2 2" />
                 <path d="M17.8 20.817l-2.172 1.138a.392 .392 0 0 1 -.568 -.41l.415 -2.411l-1.757 -1.707a.389 .389 0 0 1 .217 -.665l2.428 -.352l1.086 -2.193a.392 .392 0 0 1 .702 0l1.086 2.193l2.428 .352a.39 .39 0 0 1 .217 .665l-1.757 1.707l.414 2.41a.39 .39 0 0 1 -.567 .411l-2.172 -1.138z" />
               </svg>
-              <span className="text-lg font-semibold sm:inline-block">
+              <span className="text-foreground text-lg font-semibold sm:inline-block">
                 Image Compressor
               </span>
             </a>
