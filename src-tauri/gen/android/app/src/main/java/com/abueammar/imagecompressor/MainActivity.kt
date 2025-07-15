8887270bb5101b97f72f417fc4569d485ccf2b7f
abue-ammar <iabueammar@gmail.com>
2025-07-15 12:14:23 +0600

    feat: update Android theme to follow system theme and enable edge-to-edge display

--- src-tauri/gen/android/app/src/main/java/com/abueammar/imagecompressor/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/abueammar/imagecompressor/MainActivity.kt
@@ -1,3 +1,14 @@
 package com.abueammar.imagecompressor
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import app.tauri.activity.TauriActivity
+
+class MainActivity : TauriActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Enable edge-to-edge display
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
